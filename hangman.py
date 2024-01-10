fbf2703071d3ce314e9b64aed0c5aa86919d1000
AleksStein <losttape1@gmail.com>
2024-01-10 17:57:13 +0300

    Коментарий

---
+++ hangman.py
@@ -0,0 +1,16 @@
+mass = [5,4,3,2,1,6,8,7,10,9,13,12,11,15,14]
+
+c1 = 0
+l = len(mass)-1
+
+while c1 < l:
+
+    c2 = 0
+    while c2 < l-c1:
+        if mass[c2] > mass[c2+1]:
+            mass[c2], mass[c2+1] = mass[c2+1], mass[c2]
+        c2 += 1
+
+    c1 += 1
+
+print(mass)
